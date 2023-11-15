f6dc43bde3ab71447fc5cdbf86e528345a4da458
m220107 <220107ie@yse-c-net>
2023-11-15 10:18:25 +0900

    先生の回答追加

--- message.js
+++ message.js
@@ -1,13 +1,20 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
 
-dotenv.config();
-const name = process.env.NAME;
-const number = process.env.NUNBER;
-
-
-function create(name, number) {
-    const result = `${name}の学籍番号は${number}です。`;
-  
-    return result;
+exports.create = (name, number) => {
+    var message = `${name}の学籍番号は${number}です。`;
+    return message;
   }
+
+
+// const fs = require('fs');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+// const name = process.env.NAME;
+// const number = process.env.NUNBER;
+
+
+// function create(name, number) {
+//     const result = `${name}の学籍番号は${number}です。`;
+  
+//     return result;
+//   }
